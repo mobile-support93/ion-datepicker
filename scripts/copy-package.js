20aaee3bc4aef411cebe56b02af2dc7256a1f8c4
mobile-support93 <mobile-support93@users.noreply.github.com>
2019-06-19 18:58:49 +0530

    Update copy-package.js

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -8,4 +8,7 @@ if (! packageJson) {
 
 delete packageJson.devDependencies;
 
-fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2),function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}););
